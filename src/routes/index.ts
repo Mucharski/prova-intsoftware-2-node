45158e4e041cedc8ddfe7b64e3d1f46af0927da7
Igor Mucharski <igor.m99@hotmail.com>
2023-06-17 10:22:37 -0300

    updating method to listen to queue. now the code is always listening to new messages outside of a route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,83 +4,9 @@ import { Router, Response, Request } from "express";
 import { formatISO, lastDayOfMonth, lastDayOfWeek } from "date-fns";
 import axios from "axios";
 import https from "https";
-import client, { Connection, Channel, ConsumeMessage } from "amqplib";
 import { Kafka, Producer } from "kafkajs";
 
 export const router = Router();
-
-router.post("/createSchedules", async (req: Request, res: Response) => {
-  const today = new Date();
-  const messages: RestaurantResponse[] = [];
-
-  const consumer =
-    async (channel: Channel) =>
-    (msg: ConsumeMessage | null): void => {
-      if (msg) {
-        let message: RestaurantResponse = JSON.parse(msg.content.toString());
-        messages.push(message);
-        channel.ack(msg);
-      }
-    };
-
-  const connection: Connection = await client.connect("amqp://localhost");
-
-  const channel: Channel = await connection.createChannel();
-
-  await channel.assertQueue("new-restaurants", { durable: false });
-
-  await channel.consume("new-restaurants", await consumer(channel));
-
-  const kafkaClient: Kafka = new Kafka({
-    clientId: "node-kafka",
-    brokers: ["localhost:9092"],
-  });
-
-  const producer: Producer = kafkaClient.producer();
-
-  messages.forEach(async (element: RestaurantResponse) => {
-    await prisma.schedule.create({
-      data: {
-        scheduledTo: lastDayOfWeek(today),
-        restaurantName: element.Name,
-        restaurantId: element.Id,
-      },
-    });
-
-    await prisma.schedule.create({
-      data: {
-        scheduledTo: lastDayOfMonth(today),
-        restaurantName: element.Name,
-        restaurantId: element.Id,
-      },
-    });
-
-    await producer.connect();
-
-    console.log("Enviando mensagem ao Kafka...");
-    await producer.send({
-      topic: "schedules",
-      messages: [
-        {
-          value: JSON.stringify(element),
-        },
-      ],
-    });
-    console.log("Mensagem enviada ao Kafka!");
-  });
-
-  await producer.disconnect();
-
-  if (messages.length > 0) {
-    return res.status(200).json({
-      message: "A fila foi processada e os agendamentos foram criados!",
-    });
-  } else {
-    return res.status(400).json({
-      message: "A fila estava vazia e nenhum agendamento foi criado!",
-    });
-  }
-});
 
 router.get("/getSchedules", async (req: Request, res: Response) => {
   const schedules = await prisma.schedule.findMany();
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,41 @@
+import { RestaurantResponse } from "./interfaces/createSchedule";
 import express from "express";
 import { router } from "./routes";
+import client, { Connection, Channel, ConsumeMessage } from "amqplib";
+import { processMessage } from "./utils/processMessage";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+messageConsumer();
+async function messageConsumer(): Promise<void> {
+  const consumer =
+    async (channel: Channel) =>
+    (msg: ConsumeMessage | null): void => {
+      if (msg) {
+        console.log(msg.content.toString());
+        let restaurantResponse: RestaurantResponse = JSON.parse(
+          msg.content.toString()
+        );
+        processMessage(restaurantResponse);
+        channel.ack(msg);
+      }
+    };
+
+  const connection: Connection = await client.connect("amqp://localhost");
+
+  const channel: Channel = await connection.createChannel();
+
+  await channel.assertQueue("new-restaurants", { durable: false });
+
+  console.log("Waiting for messages....");
+
+  await channel.consume("new-restaurants", await consumer(channel));
+
+  //   await channel.close();
+  //   await connection.close();
+}
 
 app.listen(process.env.PORT || 3001, () => console.log("Listening!"));
